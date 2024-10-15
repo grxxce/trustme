4532d421b1da544c74573b1ea2d5fad02e7ae4ef
Grace Li <grace.l5678t@gmail.com>
2024-10-15 12:39:25 -0400

    format changes

--- client/src/components/VoiceBot.jsx
+++ client/src/components/VoiceBot.jsx
@@ -101,11 +101,11 @@ export default function VoiceBot() {
                 </radialGradient>
               </defs>
             </svg>
-          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <div className="justify-center mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 className="justify-center text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
               TrustMe
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
+            <p className="justify-center text-center mt-6 text-lg leading-8 text-gray-300">
               Listen to what I have to say.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
