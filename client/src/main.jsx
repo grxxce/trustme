c6cc2a69cd93ead9093ff9bd473cd8ec49f7510d
Grace Li <graceli.contact@gmail.com>
2024-10-15 12:38:03 -0400

    backend

---
+++ client/src/main.jsx
@@ -0,0 +1,29 @@
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import VoiceBot from "./components/VoiceBot";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <VoiceBot />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
