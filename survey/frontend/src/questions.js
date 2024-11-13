ebc335c8a642b8c629d26bb2f5691ee0f41768d4
Nathan Li <moncskoli@gmail.com>
2024-11-13 11:02:32 -0500

    new questions

--- survey/frontend/src/questions.js
+++ survey/frontend/src/questions.js
@@ -1,5 +1,5 @@
 export const questionList = [
-  "If you flipped a coin, would you want heads or tails?",
-  "A close friend asks for your opinion on their recent change in appearance, but you don't think it's good. Would you be fully honest or lie to protect their feelings?",
-  "Your romantic partner wants to spend more time with you, but your friends feel neglected and want you to hang out with them. Would you spend more time with your partner or your friends?",
+  "Would you rather have a pause or rewind button in your life?",
+  "Would you rather get a million dollars right now or $100,000 every year for 10 years?",
+  "Would you rather live in a world where there’s no crime or no privacy?",
 ];
