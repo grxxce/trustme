dbad5f231a65dd4fde7a1cb7a19b05bec8559b82
Nathan Li <moncskoli@gmail.com>
2024-11-13 20:59:20 -0500

    apostrophe

--- survey/frontend/src/questions.js
+++ survey/frontend/src/questions.js
@@ -1,5 +1,5 @@
 export const questionList = [
   "Would you rather have a pause or rewind button in your life?",
   "Would you rather get a million dollars right now or $100,000 every year for 10 years?",
-  "Would you rather live in a world where there’s no crime or no privacy?",
+  "Would you rather live in a world where there's no crime or no privacy?",
 ];
